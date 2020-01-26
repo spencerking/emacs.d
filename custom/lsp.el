db0818c44893b06b3d06e6ad77f5bcb89e6f5c41
Spencer King <spencerking@users.noreply.github.com>
2020-01-25 21:15:53 -0600

    Fresh start

--- custom/lsp.el
+++
@@ -1,4 +0,0 @@
-(defun do-open-def-in-new-window ()
-  (interactive)
-  (split-window-right)
-  (lsp-find-definition))
--- custom/org.el
+++
@@ -1,7 +0,0 @@
-(defun do-org-show-all-inline-images ()
-  (interactive)
-  (org-display-inline-images t t))
-
-(global-set-key (kbd "C-c C-x C-v")
-		'do-org-show-all-inline-images)
-
